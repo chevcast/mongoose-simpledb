3c1f6af8975081224b95ac361530dfd684b38b6d
Pavel <picnixer@gmail.com>
2018-01-25 15:51:06 +0500

    mongoose 5.0 support

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ module.exports = exports = {
                     incrementBy: 1 // The number by which to increment the count each time.
                 },
                 //default options of connect, can be extended, or changed
-                options: { server: { socketOptions: { keepAlive: 1 } } }
+                options: { keepAlive: true }
             };
 
         switch (arguments.length) {
